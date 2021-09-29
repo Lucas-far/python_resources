73c5158a82abc3c23b2ad622beb246555cbf0887
Lucas-far <mizoguchia@hotmail.com>
2021-09-29 19:36:40 -0300

    no need to explain

---
+++ @python_relacionados/sqlserver/aula_5.py
@@ -0,0 +1,26 @@
+
+
+def fonte():
+    """
+    SQL Server para Programadores/Desenvolvedores
+    Seção 2: Setup
+    5. Instalação do SQL Server Management Studio
+    """
+
+
+# Download da ferramenta que faz o uso das instâncias de servidor (SQL Management Studio)
+def parte_1():
+    """
+    Google || sql management studio download
+    Título || Baixar o SQL Server Management Studio (SSMS) - Microsoft...
+    Url    || https://docs.microsoft.com/pt-br/sql/linux/quickstart-install-connect-ubuntu?view=sql-server-ver15
+    Info   || Descendo um pouco a página, há um link [  Baixar o SQL Server Management Studio (SSMS) ]. Clique nele
+    Info 2 || No próprio instalador do [ SQL Server ] há um botão para instalar chamado [ Instalar o SSMS ]
+    """
+
+
+# Instalação do SQL Management Studio
+def parte_2():
+    """
+    Local padrão: C:\Program Files (x86)\Microsoft SQL Server Management Studio 18 -> Install
+    """
